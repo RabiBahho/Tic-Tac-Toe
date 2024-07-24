ec73f702f5e3338dc974ed06d659e9bf1ab706bf
Rabibahho <rabi.github@hotmail.com>
2024-07-24 19:11:35 +0200

    NameEditingComponent

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const Player = ({ initialName, symbol }) => {
+   const [isEditing, setIsEditing] = useState(false);
+   const [playerName, setPlayerName] = useState(initialName);
+   function handleEditClick() {
+      setIsEditing((prev) => !prev);
+   }
+
+   return (
+      <li>
+         <span className='player'>
+            {isEditing ? (
+               <input
+                  type='text'
+                  required
+                  value={playerName}
+                  onChange={(e) => setPlayerName(e.target.value)}
+                  autoFocus
+               ></input>
+            ) : (
+               <span className='player-name'>{playerName}</span>
+            )}
+
+            <span className='player-symbol'>{symbol}</span>
+         </span>
+         <button onClick={handleEditClick}>
+            {isEditing ? 'Save' : 'Edit'}
+         </button>
+      </li>
+   );
+};
+export default Player;
