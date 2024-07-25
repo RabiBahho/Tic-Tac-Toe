964bcd59644f0a0e205f0ab9cf22b5f7b7af919a
Rabibahho <rabi.github@hotmail.com>
2024-07-25 11:03:55 +0200

    logic for selecting Squares

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+const initialGameBoard = [
+   [null, null, null],
+   [null, null, null],
+   [null, null, null],
+];
+const GameBoard = ({ onSelectSquare, updatedTurns }) => {
+   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+   // function handleSelectSquare(rowIndex, colIndex) {
+   //    setGameBoard((prevGameBoard) => {
+   //       const updatedBoard = [
+   //          ...prevGameBoard.map((innerArray) => [...innerArray]),
+   //       ];
+   //       updatedBoard[rowIndex][colIndex] = activePlayer;
+   //       return updatedBoard;
+   //    });
+   //    onSelectSquare();
+   // }
+   return (
+      <ol id='game-board'>
+         {updatedTurns.map((row, rowIndex) => (
+            <li key={rowIndex}>
+               <ol>
+                  {row.map((playerSymbol, colIndex) => (
+                     <li key={colIndex}>
+                        <button
+                           onClick={() => onSelectSquare(rowIndex, colIndex)}
+                        >
+                           {playerSymbol}
+                        </button>
+                     </li>
+                  ))}
+               </ol>
+            </li>
+         ))}
+      </ol>
+   );
+};
+export default GameBoard;
